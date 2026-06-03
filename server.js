83da06d3eb91c466822425b98cb37398ef59d9d6
Ravi Kumar <srbanda96@gmail.com>
2026-06-03 18:44:32 +0530

    fix

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const app = express();
 
 // Set CORS for frontend URL / allow single-node deploy
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: [
+    "https://shoping-website-i9nh.vercel.app"
+  ],
   credentials: true
 }));
 
@@ -24,17 +26,9 @@ app.use('/api/payment', require('./routes/paymentRoutes'));
 app.use('/api/analytics', require('./routes/analyticsRoutes'));
 
 // Serve frontend in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-  
-  app.use((req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
-  });
-} else {
-  app.get('/', (req, res) => {
-    res.send('ShopNest API is running in Development mode...');
-  });
-}
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
